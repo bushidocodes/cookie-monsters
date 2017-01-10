725498da022e356dff7bd98c2b3aa3905b43468b
rachel-rocks-at-ruby <rachelb@gmail.com>
2017-01-10 11:50:09 -0800

    category model and basic routes

---
+++ server/categories.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const db = require('APP/db')
+const Category = require('../db/models/category')
+
+module.exports = require('express').Router()
+  .get('/', (req, res, next) =>
+    Category.findAll()
+    .then(categories => res.json(categories))
+    .catch(next))
+  .post('/', (req, res, next) =>
+    Category.create(req.body)
+    .then(category => res.json(category))
+    .catch(next))
