880aeeee9608655c7e361ab6a99be30ad5e7a324
Sean McBride <spmcbride1201@gmail.com>
2017-01-11 15:54:16 -0500

    feat: Add get single product by ID

--- server/products.js
+++ server/products.js
@@ -6,9 +6,19 @@ const Product = require('../db/models/product')
 module.exports = require('express').Router()
   .get('/', (req, res, next) =>
     Product.findAll()
-    .then(products => res.json(products))
-    .catch(next))
+      .then(products => res.json(products))
+      .catch(next))
   .post('/', (req, res, next) =>
     Product.create(req.body)
-    .then(product => res.json(product))
-    .catch(next))
+      .then(product => res.json(product))
+      .catch(next))
+  .get('/:id', (req, res, next) => {
+    let idAsNumber = parseInt(req.params.id, 10);
+    if (!isNaN(idAsNumber)) {
+    Product.findById(idAsNumber)
+      .then((product) => res.json(product))
+      .catch(next)
+    } else {
+      res.sendStatus(400)
+    }
+  })
