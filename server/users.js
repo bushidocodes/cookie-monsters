f7134f1a10a25fa483f93d4a7173336aaa94ff58
Sean McBride <spmcbride1201@gmail.com>
2017-01-14 17:52:31 -0500

    Implemented param to DRY us users

--- server/users.js
+++ server/users.js
@@ -8,13 +8,27 @@ const {mustBeLoggedIn, forbidden} = require('./auth.filters');
 module.exports = require('express').Router()
 	.get('/', forbidden('only admins can list users'), (req, res, next) =>
 		User.findAll()
-		.then(users => res.json(users))
-		.catch(next))
+			.then(users => res.json(users))
+			.catch(next))
 	.post('/', (req, res, next) =>
 		User.create(req.body)
-		.then(user => res.status(201).json(user))
-		.catch(next))
-	.get('/:id', mustBeLoggedIn, (req, res, next) =>
+			.then(user => res.status(201).json(user))
+			.catch(next))
+
+	// Use Param to DRY subsequent routes...
+	// Implemented this when I thought there would be multiple routes
+	.param('id', mustBeLoggedIn, function (req, res, next) {
 		User.findById(req.params.id)
-		.then(user => res.json(user))
-		.catch(next));
+			.then(user => {
+				if (user) {
+					req.user = user;
+					next();
+				} else {
+					res.sendStatus(404);
+				}
+			})
+			.catch(next);
+	})
+
+	.get('/:id', (req, res, next) =>
+		res.status(200).json(req.user))
