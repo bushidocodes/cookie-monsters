e993b0e93eabb713250fa7eabddaae48c2e6e83c
rachel-rocks-at-ruby <rachelb@gmail.com>
2017-01-14 10:30:05 -0800

    #71 codeshp testing is killing me

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-// Fails Codeship test if I add the class btn-primary to the submit button. Why?
-
 export const Login = ({ login }) => (
   <form onSubmit={evt => {
     evt.preventDefault()
@@ -9,7 +7,7 @@ export const Login = ({ login }) => (
   } }>
     <input className="authFormField" name="username" />
     <input className="authFormField" name="password" type="password" />
-    <input className="btn" type="submit" value="Login" />
+    <input className={"btn" + " " + "btn-primary"} type="submit" value="Login" />
   </form>
 )
 
