4c1ce5069719eea101ce8c60ff4b23fa560f3b64
Sean McBride <spmcbride1201@gmail.com>
2017-01-16 20:15:29 -0500

    feat: Clear localStorage and Redux on logout.

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -1,10 +1,22 @@
 import { combineReducers } from 'redux'
 
-const rootReducer = combineReducers({
+// Implement Dan Abramov's suggested solution for reseting a redux store on logout
+// Added a localStorage.clear() to make sure that localStorage gets cleared out too
+// http://stackoverflow.com/questions/35622588/how-to-reset-the-state-of-a-redux-store
+
+const appReducer = combineReducers({
   auth: require('./auth').default,
   products: require('./products').default,
   cart: require('./cart').default,
   users: require('./users').default
 })
+
+const rootReducer = (state, action) => {
+  if (action.type === 'AUTHENTICATED' && action.user === '') {
+    localStorage.clear();
+    state = undefined;
+  }
+  return appReducer(state, action)
+}
 
 export default rootReducer
